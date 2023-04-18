5d36f9a21aa993e1c86c12d23cf0d209cee234f3
anshu <799147057@qq.com>
2023-04-18 13:46:28 +0800

    首页docker

--- README.md
+++ README.md
@@ -22,3 +22,10 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### CSS
+SCSS BEM(block__element--modifier)
+
+## .eslintrc.js
+关闭eslint multi命名校验
+rules 'vue/multi-word-component-names': 'off'
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,71 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    container
+  </div>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe674;</div>
+      <div class="title">
+        首页
+      </div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe600;</div>
+      <div class="title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe601;</div>
+      <div class="title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe604;</div>
+      <div class="title">我的</div>
+    </div>
+  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<style lang="scss" scoped>
+  .container{
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: .5rem;
+    width: 100%;
+    box-sizing: border-box;
+    background-color: #F1F1F1;
+  }
+  .docker{
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    display: flex;
+    width: 100%;
+    height: .5rem;
+    padding: 0 .18rem;
+    box-sizing: border-box;
+    border-top: .01rem solid #F1F1F1 ;
+    &__item{
+      flex: 1;
+      text-align: center;
+      color: #000;
+      .iconfont{
+        font-size: .2rem;
+        margin: .07rem 0 .02rem 0;
+      }
+      .title{
+        font-size: .2rem;
+        transform: scale(.5, .5);
+        transform-origin: center top;
+        a{
+          text-decoration: none;
+        }
+      }
+      &--active{
+        color: #1FA4FC;
+        a{
+          color: #1FA4FC;
+        }
+      }
     }
   }
-}
 </style>
