696bc1b87b948ca1aae06edc54b6c4463c14daa4
anshu <799147057@qq.com>
2023-04-19 10:42:34 +0800

    App.vue get current route

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <TabBar v-if="isShow" />
 </template>
 <script lang="ts">
+// import { useRouter } from 'vue-router'
 import TabBar from './components/TabBar.vue'
 
 export default {
@@ -10,11 +11,37 @@ export default {
   components: {
     TabBar
   },
+  // data () {
+  //   return {
+  //     tabShow: true
+  //   }
+  // },
+  // 第一种，使用computed和this.$route.name
   computed: {
     isShow () {
       const disabledTabRoutes = ['login']
       return !disabledTabRoutes.includes(this.$route.name)
     }
   }
+  // 第二种，使用mounted和window.location
+  // mounted () {
+  //   const path = window.location.hash
+  //   this.tabShow = !(path.indexOf('login') > 0)
+  // }
+  // 第三种，使用watch监听$route
+  // watch: {
+  //   // 写法1
+  //   // $route: {
+  //   //   handler (newValue, oldValue) {
+  //   //     console.log(newValue, oldValue)
+  //   //     console.log(this.$route.name)
+  //   //   }
+  //   // },
+  //   // 写法2
+  //   '$route' (newValue, oldValue) {
+  //     const name = newValue.name
+  //     console.log(name)
+  //   }
+  // }
 }
 </script>
